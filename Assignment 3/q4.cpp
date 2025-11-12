8de9bf26d0e7183890357fff5fd44bbc5a1e1533
Vedeesh Bhalla <vedeesh.bhalla12@gmail.com>
2025-11-12 14:11:35 +0530

    Implement infix to postfix conversion in q4.cpp

---
+++ Assignment 3/q4.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+int precedence(char c) {
+    if (c == '^')
+        return 3;
+    else if (c == '*' || c == '/')
+        return 2;
+    else if (c == '+' || c == '-')
+        return 1;
+    else
+        return -1;
+}
+
+string infixToPostfix(string s) {
+    stack<char> st;
+    string result;
+
+    for (int i = 0; i < s.length(); i++) {
+        char c = s[i];
+
+        if (isalnum(c))
+            result += c;
+
+        else if (c == '(')
+            st.push(c);
+
+        else if (c == ')') {
+            while (!st.empty() && st.top() != '(') {
+                result += st.top();
+                st.pop();
+            }
+            st.pop();
+        }
+
+        else {
+            while (!st.empty() && precedence(c) <= precedence(st.top())) {
+                result += st.top();
+                st.pop();
+            }
+            st.push(c);
+        }
+    }
+
+    while (!st.empty()) {
+        result += st.top();
+        st.pop();
+    }
+
+    return result;
+}
+
+int main() {
+    string exp;
+    cout << "Enter infix expression: ";
+    cin >> exp;
+
+    cout << "Postfix expression: " << infixToPostfix(exp) << endl;
+    return 0;
+}
