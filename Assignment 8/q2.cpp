0f50809de06388f470777ce8b636283cafcc98d2
Vedeesh Bhalla <vedeesh.bhalla12@gmail.com>
2025-12-05 12:30:28 +0530

    Update q2.cpp

--- Assignment 8/q2.cpp
+++ Assignment 8/q2.cpp
@@ -52,42 +52,6 @@ Node* findMin(Node* root){
         root = root->left;
     return root;
 }
-
-Node* inorderSuccessor(Node* root, int key){
-    Node* curr = searchRec(root, key);
-    if(curr == NULL) return NULL;
-
-    if(curr->right) return findMin(curr->right);
-
-    Node* succ = NULL;
-    Node* ancestor = root;
-    while(ancestor != curr){
-        if(key < ancestor->data){
-            succ = ancestor;
-            ancestor = ancestor->left;
-        }
-        else ancestor = ancestor->right;
-    }
-    return succ;
-}
-Node* inorderPredecessor(Node* root, int key){
-    Node* curr = searchRec(root, key);
-    if(curr == NULL) return NULL;
-
-    if(curr->left) return findMax(curr->left);
-
-    Node* pred = NULL;
-    Node* ancestor = root;
-    while(ancestor != curr){
-        if(key > ancestor->data){
-            pred = ancestor;
-            ancestor = ancestor->right;
-        }
-        else ancestor = ancestor->left;
-    }
-    return pred;
-}
-
 int main(){
     Node* root = NULL;
     int arr[] = {20, 8, 22, 4, 12, 10, 14};
